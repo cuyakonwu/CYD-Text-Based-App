0be312cad73adc0258e8bf1621c4aec1f9585d00
cuyakonwu <cuyakonwu@uchicago.edu>
2023-06-28 15:00:56 -0400

    Completed comments on shell-code

--- shell-code.py
+++ shell-code.py
@@ -1,18 +1,24 @@
+#Import Flask and Twilio Libraries
 from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 
+#Initialize the app in flask
 app = Flask(__name__)
 
 
 @app.route('/bot', methods=['POST'])
 def bot():
+    #Stores the incoming message as lower case
     incoming_msg = request.values.get('Body', '').lower()
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
 
-    #Set a string of conditionals that access a website
+    """
+    Set a string of conditionals that access a website
+    Once the message is answered, responded is set to True
+    """
 
     if not responded:
         msg.body('I only know about famous quotes and cats, sorry!')
